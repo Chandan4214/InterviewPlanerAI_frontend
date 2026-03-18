ebc8d462792bcb377c994c29c01c336c4dc6acc9
Chandan <cs762585@gmail.com>
2026-03-18 14:13:52 +0530

    protected route fixed

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -1,7 +1,7 @@
-import {createBrowserRouter} from "react-router-dom";
-import Login from "./features/auth/pages/Login";
-import Register from "./features/auth/pages/Register";
-import Protected from "./features/auth/components/protected";
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./features/auth/pages/Login.jsx";
+import Register from "./features/auth/pages/Register.jsx";
+import Protected from "./features/auth/components/Protected.jsx";
 import Home from "./features/interview/pages/Home.jsx";
 import Interview from "./features/interview/pages/Interview.jsx";
 
@@ -9,19 +9,26 @@ import Interview from "./features/interview/pages/Interview.jsx";
 export const router = createBrowserRouter([
   {
     path: "/login",
-    element: <Login/>
+    element: <Login />,
   },
-   {
+  {
     path: "/register",
-    element: <Register/>
+    element: <Register />,
   },
   {
     path: "/",
-    element:  <Protected><Home/></Protected>
+    element: (
+      <Protected>
+        <Home />
+      </Protected>
+    ),
   },
   {
     path: "/interview/:interviewId",
-    element:  <Protected><Interview/></Protected>
-    
-  }
-])
+    element: (
+      <Protected>
+        <Interview />
+      </Protected>
+    ),
+  },
+]);
